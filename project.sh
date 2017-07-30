edd11db42cc2105f19cc8385c6fb523262f27286
Michal Augustyn <michal.augustyn@nokia.com>
2017-07-30 14:04:00 +0200

    Refactored code

--- project.sh
+++ project.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-path=/home/jac/shared/costu
-
-mkdir ${path}
-echo "drugie echooooo" > ${path}/plik
 
 export PS1="[\[\e[35m\]\A\[\e[m\]] \[\e[33m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
+
+path=/home/jac/shared/costu
+mkdir ${path}
 
