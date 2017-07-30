80da837c872efb5933472bc98c52d922075de8e2
Michal Augustyn <michal.augustyn@nokia.com>
2017-07-30 13:50:04 +0200

    Adding prompt configuration

--- project.sh
+++ project.sh
@@ -5,3 +5,5 @@ path=/home/jac/shared/costu
 mkdir ${path}
 echo "drugie echooooo" > ${path}/plik
 
+export PS1="[\[\e[35m\]\A\[\e[m\]] \[\e[33m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
+
